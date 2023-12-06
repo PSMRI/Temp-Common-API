389c868693571839ade7faee3f8f27d5927363a0
appuser <appuser@amritwprdev.piramalswasthya.org>
2023-12-06 18:03:34 +0530

    WASA change

--- src/main/java/com/iemr/common/config/InterceptorConfig.java
+++ src/main/java/com/iemr/common/config/InterceptorConfig.java
@@ -35,15 +35,15 @@ public class InterceptorConfig extends WebMvcConfigurerAdapter
 	@Autowired
 	HTTPRequestInterceptor requestInterceptor;
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry)
-	{
-		registry.addInterceptor(requestInterceptor);
-	}
-	
 //	@Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new BlockingHttpMethodInterceptor())
-//                .addPathPatterns("/**"); 
-//    }
+//	public void addInterceptors(InterceptorRegistry registry)
+//	{
+//		registry.addInterceptor(requestInterceptor);
+//	}
+	
+	@Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new BlockingHttpMethodInterceptor())
+                .addPathPatterns("/**"); 
+    }
 }
