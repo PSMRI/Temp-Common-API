7b3b9907d10c2e311a28b0cdf59938f6df9a0202
SR20290919 <SR20290919@APL-PG02PW8B>
2025-01-19 07:27:14 -0800

    adding cod changs to fetch unallocated grievance count

---
+++ src/main/java/com/iemr/common/controller/grievance/GrievanceController.java
@@ -0,0 +1,42 @@
+package com.iemr.common.controller.grievance;
+
+
+import com.iemr.common.service.grievance.GrievanceDataSync;
+import com.iemr.common.utils.response.OutputResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+
+
+import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GrievanceController {
+	final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+    @Autowired
+    private GrievanceDataSync grievanceDataSync;
+
+    
+    @CrossOrigin()
+   	@Operation(summary = "/unallocatedGrievanceCount")
+   	@RequestMapping(value = "/unallocatedGrievanceCount", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON, headers = "Authorization")
+       public String fetchUnallocatedGrievanceCount() {
+       	OutputResponse responseData = new OutputResponse();
+       	try {
+   			responseData.setResponse(grievanceDataSync.fetchUnallocatedGrievanceCount());
+   		} catch (Exception e) {
+   			logger.error("UnallocatedGrievanceCount failed with error" + e.getMessage(), e);
+   			responseData.setError(e);
+   		}
+   return responseData.toString();
+           
+       }
+}
